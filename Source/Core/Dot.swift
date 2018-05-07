fab22c832bb62090605349748e8e2eef2b92335e
leoli <codelijie@gmail.com>
2018-05-07 17:25:15 +0800

    add dot

---
+++ Source/Core/Dot.swift
@@ -0,0 +1,13 @@
+//
+//  Dot.swift
+//  GPassword
+//
+//  Created by leoli on 2018/5/7.
+//  Copyright © 2018年 leoli. All rights reserved.
+//
+
+import UIKit
+
+class Dot: CAShapeLayer {
+    // contain muti CAShapeLayers to draw
+}
